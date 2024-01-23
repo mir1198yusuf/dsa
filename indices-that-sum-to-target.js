deab15d23a11b457ca0eeee757c060f2356019c5
mir1198yusuf <34382235+mir1198yusuf@users.noreply.github.com>
2024-01-23 23:08:40 +0530

    fixed indices-that-sum-to-target to improve more

--- indices-that-sum-to-target.js
+++ indices-that-sum-to-target.js
@@ -1,19 +1,23 @@
-const array = [3,2,4,2,5,6,8,45]
-const target = 53
+const array1 = [3, 2, 4, 2, 5, 6, 8, 45];
+const target1 = 53;
 
-function magic(a,t) {
-    const obj = {}
-    for (let i=0; i<a.length; i++) {
-        obj[a[i]] = i
+const array2 = [3, 2, 4];
+const target2 = 6;
+
+function magic(a, t) {
+    const obj = {};
+    for (let i = 0; i < a.length; i++) {
+        obj[a[i]] = i;
     }
 
-    for (let i=0; i<a.length; i++) {
+    for (let i = 0; i < a.length; i++) {
         const x = t - a[i];
-        if (obj[x]) return [i, obj[x]];
+        if (obj[x] != null && obj[x] !== i) return [i, obj[x]];
     }
 }
 
-console.log(magic(array, target))
+console.log(magic(array1, target1));
+console.log(magic(array2, target2));
 
 // space - o(n)
 // time - o(n)
