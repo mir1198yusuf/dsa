ef202900bed31381481d48ce79dfc536da596173
mir1198yusuf <34382235+mir1198yusuf@users.noreply.github.com>
2024-06-28 18:43:09 +0530

    bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,6 +1,6 @@
 function bubbleSort(a) {
     for (let i = 0; i < a.length; i++) {
-        for (let j = i; j < a.length; j++) {
+        for (let j = i+1; j < a.length; j++) {
             if (a[i] > a[j]) {
                 let t = a[i];
                 a[i] = a[j];
